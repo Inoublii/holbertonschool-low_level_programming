16a793747600d4de748699bf5012bdea94052682
inoublii <inoublii@gmail.com>
2019-09-18 14:06:49 +0100

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
-#!/bin/bash
-init main () {
-    puts ("Programming is like building a multilingual puzzle")
-    return 0;
-    }
+#include <studio.h>
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}

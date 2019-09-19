70204f14e19f8e6fce19435f8770973e00df04aa
inoublii <inoublii@gmail.com>
2019-09-19 12:25:27 +0100

    hello x

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-  int x;
-  for (x = 'a'; x <= 'z'; x++)
-    putchar (x);
-  putchar('\n');
+int x;
+for (x = 'a'; x <= 'z'; x++)
+putchar (x);
+putchar('\n');
 return 0;
 }

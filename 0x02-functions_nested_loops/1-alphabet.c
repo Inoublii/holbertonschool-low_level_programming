473a83312ec7ad021a795c005f19a2bd7fd4555e
inoublii <inoublii@gmail.com>
2019-09-24 13:13:51 +0000

    hello

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- *main-prints out alphabets, followed by new line
- *print alphabets
- *Return: 0
+ * print_alphabet - entry point
+ * print alphabet
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {

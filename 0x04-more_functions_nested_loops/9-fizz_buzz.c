9b052656e0a15d7ec04ff31c69150c6504a63f1c
inoublii <inoublii@gmail.com>
2019-09-26 18:05:30 +0000

    hello

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,9 +14,9 @@ printf("BUZZ");
 else if	((x % 3 == 0) && (x % 5 == 0))
 printf("FizzBuzz");
 else if	(x % 3 == 0)
-printf("Fizz");
-else if (x % 5 == 0)
-printf("Buzz");
+printf("Fizz ");
+else if ((x % 5 == 0) && (x != 100))
+printf("Buzz ");
 else
 printf("%d ", x);
 }

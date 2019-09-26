f040bc01bd3ae6b69551023a1a50ae22ba636525
inoublii <inoublii@gmail.com>
2019-09-26 14:30:03 +0000

    hello code

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
 /**
  *_isupper - checks fo uppercases
- *@c: character to check 
+ *@c:character to check 
  *Return: 0 or 1.
 */
 int _isupper(int c)
 {
-	if (c >= 'A' && c >= 'Z')
+if (c >= 'A' && c >= 'Z')
 		return (1);
 	else
 		return (0);
-}
-	
+}	
 
 
 

73fd28772421b9a94e3b76a28e3fa2e771610886
inoublii <inoublii@gmail.com>
2019-09-26 17:56:42 +0000

    getting used to

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+   * main - check the code for Holberton School students.
+    *
+     * Return: Always 0.
+      */
+int main()
+{
+	int x;
+	for	(x =1; x <= 100; x++)
+	{
+	if	(x == 100)
+	printf ("BUZZ");
+	else if	((x % 3 == 0) && (x % 5 == 0))
+	printf ("FizzBuzz");
+	else if	(x % 3 == 0) 
+	printf ("Fizz");
+	else if (x % 5 == 0)
+	printf ("Buzz");
+	else  printf("%d ", x);
+	}
+	printf("\n");
+	return (0);
+	}

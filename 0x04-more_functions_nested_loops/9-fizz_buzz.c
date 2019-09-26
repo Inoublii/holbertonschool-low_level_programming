c83cdafb90a383506d9ff858a65c432e3f936cb2
inoublii <inoublii@gmail.com>
2019-09-26 18:12:01 +0000

    hello

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ for (x = 1; x <= 100; x++)
 if (x == 100)
 printf("Buzz");
 else if	((x % 3 == 0) && (x % 5 == 0))
-printf("FizzBuzz");
+printf("FizzBuzz ");
 else if	(x % 3 == 0)
 printf("Fizz ");
 else if ((x % 5 == 0) && (x != 100))

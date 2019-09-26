b01ba0fbc89d4c77f140c07f10f047f4a3979226
inoublii <inoublii@gmail.com>
2019-09-26 18:02:00 +0000

    hell

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,21 +4,22 @@
     *
      * Return: Always 0.
       */
-int main()
+int main(void)
 {
-	int x;
-	for	(x =1; x <= 100; x++)
-	{
-	if	(x == 100)
-	printf ("BUZZ");
-	else if	((x % 3 == 0) && (x % 5 == 0))
-	printf ("FizzBuzz");
-	else if	(x % 3 == 0) 
-	printf ("Fizz");
-	else if (x % 5 == 0)
-	printf ("Buzz");
-	else  printf("%d ", x);
-	}
-	printf("\n");
-	return (0);
-	}
+int x;
+for (x = 1; x <= 100; x++)
+{
+if (x == 100)
+printf("BUZZ");
+else if	((x % 3 == 0) && (x % 5 == 0))
+printf("FizzBuzz");
+else if	(x % 3 == 0)
+printf("Fizz");
+else if (x % 5 == 0)
+printf("Buzz");
+else
+printf("%d ", x);
+}
+printf("\n");
+return (0);
+}

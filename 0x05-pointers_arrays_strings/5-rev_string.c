ec858549f02261c2626c3bb45e8ca9d52f9ecca5
inoublii <inoublii@gmail.com>
2019-10-01 16:18:12 +0000

    hello arrays

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+  *rev_string - reverses array
+  *@a:
+  *@z:
+  *@y:
+  *@s:
+ *Return: Nothing
+  */
+void rev_string(char *s)
+{
+	char y;
+	int a, z = 0;
+
+	a = 0;
+	while (s[a])
+		a++;
+	for (z = 0; z < a / 2; z++)
+	{
+		y = s[a - z - 1];
+		s[a - z - 1] = s[z];
+		s[z] = y;
+
+	}
+
+	}

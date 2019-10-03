66c76068bf827fc417f9d990e92283186b9ac5bf
inoublii <inoublii@gmail.com>
2019-10-03 22:16:27 +0000

    the +++

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,4 +1,5 @@
 int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);

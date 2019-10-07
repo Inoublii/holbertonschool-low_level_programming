9700cb4582ef619c16a517bf5246f0e50be7c96c
inoublii <inoublii@gmail.com>
2019-10-07 22:27:06 +0000

    the ++

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_diagsums - Adding the diagonals of a square matrix
+ * @a: The square matrix
+ * @size: The size of the square matrix
+ */
+void print_diagsums(int *a, int size)
+{
+int row, column, sum1 = 0, sum2 = 0;
+for (row = 0, column = 0; row < size; row++, column++)
+{
+sum1 += *(a + row * size + column);
+sum2 += *(a + row * size + (size - 1 - column));
+}
+printf("%d, %d\n", sum1, sum2);
+}

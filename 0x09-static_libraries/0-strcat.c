ac90c86ed63dc652e1ef22555629e8176bf744a6
inoublii <inoublii@gmail.com>
2019-10-13 14:40:15 +0000

    hello

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,44 +1,22 @@
 #include "holberton.h"
-
 /**
- * _strlen - return length of a string
- *
- * @s: char type
- * Return:  length of string
- */
-
-int _strlen(char *s)
-{
-	int a;
-
-	for (a = 0; s[a] != '\0'; a++)
-	{
-
-	}
-	return (a);
-}
-/**
- * _strcat - function to concatnate strings
- *
- * @dest: destination for concatnation
- *
- * @src: source of string
- *
- * Return: dest
- */
-
+   * _strcat - function that concatinates two strings.
+    * @dest: pointer
+     * @src: pointer
+      * Return: 0
+      */
 char *_strcat(char *dest, char *src)
 {
-	int dest_len;
-	int a;
-
-	dest_len = _strlen(dest);
-
-	for (a = 0 ; src[a] != '\0' ; a++)
-		dest[dest_len + a] = src[a];
-
-	dest[dest_len + a] = '\0';
-
+	int a, b;
+	b = 0;
+	while (dest[b] != '\0')
+		b++;
+	a = 0;
+	while (src[a] != '\0')
+	{
+		*(dest + b) = *(src + a);
+		a++;
+		b++;
+	}
 	return (dest);
-
 }
--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,44 +1,18 @@
 #include "holberton.h"
-
 /**
- * _strlen - return length of a string
- *
- * @s: char type
- * Return:  length of string
- */
-
-int _strlen(char *s)
-{
-	int a;
-
-	for (a = 0; s[a] != '\0'; a++)
-	{
-
-	}
-	return (a);
-}
-/**
- * _strncat - function to concatnate strings
- *
- * @dest: destination for concatnation
- * @n: byte size .
- * @src: source of string
- *
- * Return: dest
- */
-
+   * _strncat - function that concatinates two strings
+    * @dest: pointer
+     * @src: pointer
+      * @n: number of bytes
+       * Return: 0
+       */
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len;
-	int a;
-
-	dest_len = _strlen(dest);
-
-	for (a = 0 ; a < n && src[a] != '\0' ; a++)
-		dest[dest_len + a] = src[a];
-
-	dest[dest_len + a] = '\0';
-
+	int dlen, slen;
+	for (dlen = 0; dest[dlen] != '\0'; dlen++)
+		;
+	for (slen = 0; slen < n && src[slen] != '\0'; slen++)
+		dest[dlen + slen] = src[slen];
 	return (dest);
-
 }
+

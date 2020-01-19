6b910638770f955c4627a90ffe821b7ea4966027
inoublii <inoublii@gmail.com>
2020-01-19 23:35:48 +0100

    commit

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,21 +11,19 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 hash_node_t *tmp;
 unsigned int long i;
 if ((!key || !key[0]) || (!ht || !ht->array))
-{
 	return (NULL);
-}
 i = key_index((unsigned char *)key, ht->size);
 if (!ht->array[i])
 {
-	return (NULL);
+    return (NULL);
 }
 tmp = ht->array[i];
 while (tmp)
 {
 	if (strcmp(key, tmp->key) == 0)
-{
-		return (tmp->value);
-}
+	{
+			return (tmp->value);
+	}
 	tmp = tmp->next;
 }
 return (NULL);

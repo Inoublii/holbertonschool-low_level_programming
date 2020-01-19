deec91b04ac7863dd57d66975205def579dd9e90
inoublii <inoublii@gmail.com>
2020-01-19 23:29:14 +0100

    commit

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,5 +8,17 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-
+hash_node_t *tmp;
+unsigned int long i;
+if ((!key || !key[0]) || (!ht || !ht->array))
+	return (NULL);
+i = key_index((unsigned char *)key, ht->size);
+if (!ht->array[i])
+	return (NULL);
+tmp = ht->array[i];
+while (tmp)
+	if (strcmp(key, tmp->key) == 0)
+		return (tmp->value);
+	tmp = tmp->next;
+return (NULL);
 }

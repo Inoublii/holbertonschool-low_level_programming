de661910d5ef8dba1cd9d9e3c3d65afbaf21ef38
inoublii <inoublii@gmail.com>
2020-02-21 19:23:04 +0100

    fx

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """Island_perimeter function"""
+
+
 def island_perimeter(grid):
-	s = 0
-	i = 1
-	for i in [len[grid-1]]:
-		for j in [len[grid-1]]:
-			for i,j in grid:
-				if grid[i][j] == 1:
-					if grid[i-1][j] == 0:
-						s += 1
-					if grid[i+1][j] == 0:
-						s += 1
-					if grid[i][j-1] == 0:
-						s += 1
-					if grid[i-1][j+1] == 0:
-						s += 1
-	return (s)
-
-
+    s = 0
+    i = 1
+    for i in [len[grid-1]]:
+        for j in [len[grid-1]]:
+            for i, j in grid:
+                if grid[i][j] == 1:
+                    if grid[i-1][j] == 0:
+                        s += 1
+                    if grid[i+1][j] == 0:
+                        s += 1
+                    if grid[i][j-1] == 0:
+                        s += 1
+                    if grid[i-1][j+1] == 0:
+                        s += 1
+    return (s)

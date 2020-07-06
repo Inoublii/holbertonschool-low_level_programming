476fc76694a0d2819e3a112ee2421926c106ceff
inoublii <inoublii@gmail.com>
2020-07-06 14:03:15 +0100

    sortings

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,20 @@
 #include "search_algos.h"
-#include <stdio.h>
-#include <stdlib.h>
 
+/**
+  * linear_search - a function that searches
+  * @array: pointer
+  * @size: size_t
+  * @value: int
+  * Return: int
+  */
+int linear_search(int *array, size_t size, int value) {
+size_t i;
+if (!array) {
+return (-1); }
+for (i = 0; i < size; i++) {
+printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return (i);
+}
+return (-1);
+}
